c361c97b83c4ae3c549cf466025f7a926dd87ab7
mkamilgok <mkamilgok@gmail.com>
2019-03-20 15:32:19 +0300

    add new constructor for LLNode class

--- src/textgen/MyLinkedList.java
+++ src/textgen/MyLinkedList.java
@@ -113,14 +113,17 @@ class LLNode<E>
 	LLNode<E> next;
 	E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
-
 	public LLNode(E e) 
 	{
 		this.data = e;
 		this.prev = null;
 		this.next = null;
+	}
+	
+	public LLNode(E e, LLNode<E> prev, LLNode<E> next) {
+		this.data = e;
+		this.prev = prev;
+		this.next = next;
 	}
 
 }
