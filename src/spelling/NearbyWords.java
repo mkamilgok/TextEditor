bacdd8c89150716de766c6780631e2cc7e32d5bc
mkamilgok <mkamilgok@gmail.com>
2019-04-29 16:34:01 +0300

    delete main method in NearbyWords class

--- src/spelling/NearbyWords.java
+++ src/spelling/NearbyWords.java
@@ -157,22 +157,5 @@ public class NearbyWords implements SpellingSuggest {
 
 	}	
 
-   public static void main(String[] args) {
-	   /* basic testing code to get started
-	   String word = "i";
-	   // Pass NearbyWords any Dictionary implementation you prefer
-	   Dictionary d = new DictionaryHashSet();
-	   DictionaryLoader.loadDictionary(d, "data/dict.txt");
-	   NearbyWords w = new NearbyWords(d);
-	   List<String> l = w.distanceOne(word, true);
-	   System.out.println("One away word Strings for for \""+word+"\" are:");
-	   System.out.println(l+"\n");
-
-	   word = "tailo";
-	   List<String> suggest = w.suggestions(word, 10);
-	   System.out.println("Spelling Suggestions for \""+word+"\" are:");
-	   System.out.println(suggest);
-	   */
-   }
 
 }
